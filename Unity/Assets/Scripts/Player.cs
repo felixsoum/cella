e073c40e0574e77710ff48347e64e00568461a63
felixsoum <felix@felixsoum.com>
2014-10-18 14:42:04 -0400

    Add player

---
+++ Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float moveSpeed = 100f;
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		Vector2 axisInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		rigidbody2D.velocity = axisInput * moveSpeed * Time.deltaTime;
+		animator.SetBool("IsFiring", Input.GetButton("Fire1"));
+	}
+}
