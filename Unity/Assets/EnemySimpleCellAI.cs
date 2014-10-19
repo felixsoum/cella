55066be64eca131413b8ff7af0b4a2349c292d42
psyomn <lethaljellybean@gmail.com>
2014-10-18 21:20:10 -0400

    enemy: crappy movement works

--- Unity/Assets/EnemySimpleCellAI.cs
+++ Unity/Assets/EnemySimpleCellAI.cs
@@ -5,10 +5,17 @@ public class EnemySimpleCellAI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		var TP = transform.position;
+		pos = Random.Range (0.0f, 8.0f); 
+		TP.x = pos - 4.0f;
 	}
 
-	void FixedUpdate() {
-	
+	void Update() {
+		var TP = transform.position;
+		pos += Time.deltaTime;
+		TP.x = Mathf.PingPong (pos, 8.0f) - 4.0f;
+		transform.position = TP;
 	}
+
+	private float pos; 
 }
