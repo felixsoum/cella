6638d639a33cff2a33ed50a4cd9d28f4a99fccf9
felixsoum <felix@felixsoum.com>
2014-10-19 17:21:30 -0400

    Add shoot and death sounds

---
+++ Unity/Assets/Scripts/ShootSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootSound : MonoBehaviour
+{
+	private AudioSource shootAudio;
+
+	private void Start()
+	{
+		shootAudio = GetComponent<AudioSource>();
+	}
+
+	private void PewPew()
+	{
+		shootAudio.Play();
+	}
+}
