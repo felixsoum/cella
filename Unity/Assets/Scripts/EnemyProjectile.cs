d00281be9fba4a8e4292bdbb9f7a6ca6e795d1ec
psyomn <lethaljellybean@gmail.com>
2014-10-18 22:37:42 -0400

    bullet: add basic projectile

---
+++ Unity/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var TP = transform.position;
+		TP.x -= 0.1f;
+		transform.position = TP;
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			collision.gameObject.SendMessage("ApplyDamage", stats.Attack);
+		}
+		Destroy (this.gameObject);
+	}
+
+	/* For power stats etc */
+	private Projectile stats = new Projectile();
+}
